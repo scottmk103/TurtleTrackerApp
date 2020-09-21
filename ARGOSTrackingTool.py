22788019e15ce5ef0c7d36ba80304290f8897cda
scottmk103 <scottmk103@gmail.com>
2020-09-20 20:31:07 -0400

    Add ARGOSTrackingTool file

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,9 @@
+#---------------------------------------------------------------------------------
+#ARGOSTrackingTool.py
+#
+#Description: Reads in an ARGOS tracking data file and allows the user
+#   to view the location of the turtle for a specidfied data entered
+#   via user input.
+#
+#Author: Michael Scott (mks71@duke.edu
+#Date: Fall 2020
